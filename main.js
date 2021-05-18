7693868a1d184f94258b4df0a6631feee725827a
Natalie Tso <natalietso16@gmail.com>
2021-05-17 17:24:58 -0700

    moved encryption function to separate file

---
+++ main.js
@@ -0,0 +1,11 @@
+const include = file => {
+    var script  = document.createElement('script');
+    script.src  = file;
+    script.type = 'text/javascript';
+    script.defer = true;
+
+    document.getElementsByTagName('body').item(0).appendChild(script);
+}
+
+include("./encryption.js");
+include("./script.js");
